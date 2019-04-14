e07d239d3f3315cd54ac569989e570af40d63f8f
Roberto Pintos López <roberto@secture.com>
2019-04-14 13:13:07 +0200

    Added ant manager. Added entrypoint.

--- src/test/AllTests.ts
+++ src/test/AllTests.ts
@@ -1,3 +1,5 @@
+import { AntTest } from './AntTest';
+import { AntSqlManagerTest } from './api/AntSqlManagerTest';
 import { AntSqlModelManagerTest } from './api/AntSqlModelManagerTest';
 import { ITest } from './ITest';
 import { AntSqlModelTest } from './model/AntSqlModelTest';
@@ -7,8 +9,10 @@ import { DBTestManager } from './secondary/DBTestManager';
 export class AllTest implements ITest {
   public performTests(): void {
     const deleteAllTables = new DBTestManager().deleteAllTables();
+    new AntSqlManagerTest().performTests();
     new AntSqlModelManagerTest().performTests();
     new AntSqlModelTest().performTests();
     new AntSqlSecondaryEntityManagerTest(deleteAllTables).performTests();
+    new AntTest().performTests();
   }
 }
