f819e8681a633f97fef40780a6eb8d539ad7809b
Roberto Pintos López <roberto@secture.com>
2019-10-02 17:38:10 +0200

    Added query config factory to the model manager.
    Added query config factory to the model manager.
    Model manager interface has been updated.
    Added tests.

--- src/api/AntSqlModelManager.ts
+++ src/api/AntSqlModelManager.ts
@@ -10,6 +10,7 @@ import { AntSqlSecondaryEntityManager } from '../persistence/secondary/AntSqlSec
 import { ISqlSecondaryEntityManager } from '../persistence/secondary/ISqlSecondaryEntityManager';
 import { KnexDriver } from '../persistence/secondary/KnexDriver';
 import { IAntSqlModelConfig } from './config/IAntSqlModelConfig';
+import { QueryConfigFactory } from './config/QueryConfigFactory';
 import { IAntSqlModelManager } from './IAntSqlModelManager';
 
 export class AntSqlModelManager<TEntity extends IEntity>
@@ -19,6 +20,44 @@ export class AntSqlModelManager<TEntity extends IEntity>
     IAntSqlModel,
     ISqlModelManager<TEntity>
 > implements IAntSqlModelManager<TEntity> {
+
+  /**
+   * Query config factory.
+   */
+  protected _queryConfigFactory: QueryConfigFactory<TEntity>;
+
+  /**
+   * Gets the query config factory.
+   * @returns query config factory.
+   */
+  public get cfgGen(): QueryConfigFactory<TEntity> {
+    if (!this._queryConfigFactory) {
+      throw new Error(
+`The current action could not be performed because the model manager is not ready.
+This is probably caused by the absence of a config instance. Ensure that config is set.`,
+      );
+    }
+
+    return this._queryConfigFactory;
+  }
+
+  /**
+   * Gets the current AntJS model config.
+   * @returns Current AntJS model config.
+   */
+  public config(): IAntSqlModelConfig;
+  /**
+   * Sets the current AntJS model config.
+   * @param config new AntJS model config.
+   * @returns this instance.
+   */
+  public config(config: IAntSqlModelConfig): this;
+  public config(config?: IAntSqlModelConfig): IAntSqlModelConfig|this {
+    if (undefined !== config && !this._config) {
+      this._queryConfigFactory = new QueryConfigFactory(config.knex, this._model);
+    }
+    return super.config(config);
+  }
 
   /**
    * Inserts an entity.
--- src/api/IAntSqlModelManager.ts
+++ src/api/IAntSqlModelManager.ts
@@ -2,6 +2,14 @@ import { IAntModelManager } from '@antjs/ant-js/src/api/IAntModelManager';
 import { IEntity } from '@antjs/ant-js/src/model/IEntity';
 import { ISqlInsertable } from '../persistence/primary/ISqlInsertable';
 import { IAntSqlModelConfig } from './config/IAntSqlModelConfig';
+import { QueryConfigFactory } from './config/QueryConfigFactory';
 
 export interface IAntSqlModelManager<TEntity extends IEntity>
-  extends IAntModelManager<TEntity, IAntSqlModelConfig>, ISqlInsertable<TEntity> {}
+  extends IAntModelManager<TEntity, IAntSqlModelConfig>, ISqlInsertable<TEntity> {
+
+    /**
+     * Gets the query config factory.
+     * @returns query config factory.
+     */
+    cfgGen: QueryConfigFactory<TEntity>;
+  }
