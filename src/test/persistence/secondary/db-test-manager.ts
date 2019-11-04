e8f60ad8a0e10073e1b5e929d3c55a00d646de9d
Roberto Pintos López <roberto@secture.com>
2019-11-04 23:54:47 +0100

    Updated knex and @antjs.

--- src/test/persistence/secondary/db-test-manager.ts
+++ src/test/persistence/secondary/db-test-manager.ts
@@ -83,7 +83,7 @@ END`;
       default:
         throw new Error(`Driver "${knex.client.driverName}" not supported`);
     }
-    return knex.raw<any>(query);
+    return knex.raw<any>(query) as unknown as Promise<any>;
   }
 
   /**
@@ -119,29 +119,29 @@ END`;
         knexQuery = knex('information_schema.tables')
           .select('table_name')
           .where('TABLE_TYPE', 'BASE TABLE')
-          .andWhere('table_catalog', knex.client.database());
+          .andWhere('table_catalog', knex.client.database()) as unknown as Promise<any>;
         break;
       case KnexDriver.MYSQL:
       case KnexDriver.MYSQL2:
         knexQuery = knex('information_schema.tables')
           .select('TABLE_NAME as table_name')
-          .where('table_schema', knex.client.database());
+          .where('table_schema', knex.client.database()) as unknown as Promise<any>;
         break;
       case KnexDriver.ORACLE:
       case KnexDriver.ORACLEDB:
-        knexQuery = knex('user_tables').select('table_name');
+        knexQuery = knex('user_tables').select('table_name') as unknown as Promise<any>;
         break;
       case KnexDriver.PG:
         knexQuery = knex('information_schema.tables')
           .select('table_name')
           .where('table_schema', knex.raw('current_schema()'))
-          .andWhere('table_catalog', knex.client.database());
+          .andWhere('table_catalog', knex.client.database()) as unknown as Promise<any>;
         break;
       case KnexDriver.SQLITE3:
         knexQuery = knex('sqlite_master')
           .select('name as table_name')
           .where('type', 'table')
-          .andWhereNot('table_name', 'like', 'sqlite_%');
+          .andWhereNot('table_name', 'like', 'sqlite_%') as unknown as Promise<any>;
         break;
       default:
         throw new Error(`Driver "${knex.client.driverName}" not supported`);
@@ -164,29 +164,29 @@ END`;
         knexQuery = knex('information_schema.tables')
           .select('table_name')
           .where('table_schema', 'public')
-          .limit(limitResultsSize);
+          .limit(limitResultsSize) as unknown as Promise<any>;
         break;
       case KnexDriver.MYSQL:
       case KnexDriver.MYSQL2:
         knexQuery = knex('information_schema.tables')
           .select('TABLE_NAME as table_name')
-          .limit(limitResultsSize);
+          .limit(limitResultsSize) as unknown as Promise<any>;
         break;
       case KnexDriver.ORACLE:
       case KnexDriver.ORACLEDB:
         knexQuery = knex('user_tables')
           .select('table_name')
-          .limit(limitResultsSize);
+          .limit(limitResultsSize) as unknown as Promise<any>;
         break;
       case KnexDriver.PG:
         knexQuery = knex('information_schema.tables')
           .select('table_name')
-          .limit(limitResultsSize);
+          .limit(limitResultsSize) as unknown as Promise<any>;
         break;
       case KnexDriver.SQLITE3:
         knexQuery = knex('sqlite_master')
           .select('name as table_name')
-          .limit(limitResultsSize);
+          .limit(limitResultsSize) as unknown as Promise<any>;
         break;
       default:
         throw new Error(`Driver "${knex.client.driverName}" not supported`);
